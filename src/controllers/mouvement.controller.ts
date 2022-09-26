7918bf114c37fd1a0ffb81bf75c7aaf0283b7b5d
Nyainafitiavana <ainafitiavana0120@gmail.com>
2022-09-26 15:19:31 +0300

    format response get all mouvement

--- src/controllers/mouvement.controller.ts
+++ src/controllers/mouvement.controller.ts
@@ -16,8 +16,10 @@ import StockService from '@/services/stock.service';
 import { StockEntity } from '@/entities/stock.entity';
 import { CreatestockDto } from '@/dtos/stock.dto';
 import { Produit } from '@/interfaces/produits.interface';
+import BaseController from './base.controller';
+import { ApiResponse } from '@/interfaces/response.interface';
 
-class MouvementController {
+class MouvementController extends BaseController{
     public mouvementService = new MouvementService();
     public produitService = new ProduitService();
     public detaiService = new DetailmouvementService();
@@ -32,15 +34,17 @@ class MouvementController {
 
       const findAllMouvementsData: Mouvement[] = await this.mouvementService.findAllMouvement(limit, offset);
       const findAllMouvements: Mouvement[] = await this.mouvementService.findAllMouvement(null, null);
-      const data: any = {
-        status: 200,
-        totalRows: findAllMouvements.length,
-        limit: limit,
-        page: page,
-        rows: findAllMouvementsData
-      }
+      
+      const datas: ApiResponse = await this.response(
+        true, 
+        "Get All data success", 
+        findAllMouvementsData,
+        findAllMouvements.length,
+        limit,
+        page
+      );
 
-      res.status(200).json({ data, message: 'findAll mouvement' });
+      res.status(200).json({ datas});
     } catch (error) {
       next(error);
     }
